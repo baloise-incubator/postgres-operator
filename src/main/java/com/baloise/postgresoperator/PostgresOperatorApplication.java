004000c2005a108ee16d197288688e883298c440
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2020-11-26 15:21:34 +0100

    Initial commit

---
+++ src/main/java/com/baloise/postgresoperator/PostgresOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.baloise.postgresoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresOperatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baloise/postgresoperator/PostgresOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baloise.postgresoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
